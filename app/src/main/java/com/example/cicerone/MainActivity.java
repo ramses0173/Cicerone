7ceb256950c1651bfddaedfaeede4e555cb68b3d
Elio <ramses0173>
2019-05-16 12:51:19 +0200

    Creato bottom navigation menu sulla Main Activity Creato Fragment Profilo non totalmente completo Creato Fragment Search TO DO Creato Fragment Add Event Creato Fragment My Activity TO DO

--- app/src/main/java/com/example/cicerone/MainActivity.java
+++ app/src/main/java/com/example/cicerone/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.cicerone;
 
+import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationMenuView;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+                    switch (item.getItemId()) {
+                        case R.id.navigation_search:
+                            selectedFragment = new SearchEventMainFragment();
+                            return true;
+                        case R.id.navigation_add_event:
+                            selectedFragment = new AddEventMainFragment();
+                            return true;
+                        case R.id.navigation_event:
+                            selectedFragment = new MyEventMainFragment();
+                            return true;
+                        case R.id.navigation_profile:
+                            selectedFragment = new ProfileMainFragment();
+                            return true;
+                    }
+                    return false;
+                }
+            };
+
 }
--- app/src/main/java/com/example/cicerone/SplashActivity.java
+++ app/src/main/java/com/example/cicerone/SplashActivity.java
@@ -31,5 +31,4 @@ public class SplashActivity extends Activity {
             }
         }, SPLASH_TIME_OUT);
     }
-
 }
